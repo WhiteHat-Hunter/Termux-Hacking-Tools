0775a224c67aa3ea0a6484f1a5fddfff016edeeb
Siddhesh Surve <59648819+Technical-Sid@users.noreply.github.com>
2020-07-31 13:15:18 +0100

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+echo $red 
+ echo ""----------------------------------------------------------------------------- ""
+echo " This Script is written by Prahlad Shukla now start"
+echo "---------------------------------------------------------------------------------"
+echo "."
+
+echo  "#####################################"
+echo   " Learn Free Hacking"               
+ echo   " Subscribe to YouTube Channel The Cyber Kiddie"                                                                    
+echo   "####################################"
+
+echo "."
+git clone https://github.com/IAmBlackHacker/Facebook-BruteForce
+mv Facebook-BruteForce $HOME
+cd ; ls ; cd Facebook-BruteForce ; chmod +x * ; pip3 install requests bs4 ; pip install mechanize 
+git  clone https://github.com/JTECHCODE/crackinsta
+mv crackinsta $HOME 
+cd ; cd crackinsta ; chmod +x * ; 
+git clone https://github.com/Mebus/cupp
+mv cupp $HOME
+cd ; cd cupp ; chmod +x * ;
+git clone https://github.com/Gameye98/Lazymux
+mv Lazymux $HOME
+cd ; cd Lazymux ; chmod +x * ;
+git clone https://github.com/rajkumardusad/Tool-X
+mv Tool-X $HOME
+cd ; cd Tool-X ; chmod +x * ; 
+git clone https://github.com/Ranginang67/DarkFly-Tool
+mv DarkFly-Tool $HOME
+cd ; cd DarkFly-Tool ; chmod +x * ;
+git clone https://github.com/noob-hackers/ipdrone
+mv ipdrone $HOME
+cd ; cd ipdrone ; chmod +x * ;
+git clone https://github.com/htr-tech/zphisher
+mv zphisher $HOME
+cd ; cd zphisher ; apt install git curl php openssh -y ; chmod +x * ; 
+git clone https://github.com/websploit/websploit
+mv websploit $HOME
+cd ; cd websploit ; chmod +x * , pip install requirements.txt ; 
+cd 
+rm -rf hackersal
+echo " DONE! SUBSCRIBE TO THE YOUTUBE CHANNEL THE CYBER KIDDIE "
+exit
